efd400cda6826a7893415e3cc5f8b3235abb95c1
ADICODZZ <aditya09sinha264@gmail.com>
2025-07-23 01:10:04 +0530

    some more issues

--- controllers/preferencesController.js
+++ controllers/preferencesController.js
@@ -219,8 +219,8 @@ exports.updatePreferences = async (req, res) => {
 
       const favoriteChannels = [...preferences?.youtube?.selectedTags];
       console.log(favoriteChannels, "favoriteChannels");
-      const videoStyle = [...preferences?.youtube?.videoStyle?.selectedPredefinedTags];
-      console.log(videoStyle, "videoStyle");
+      // const videoStyle = [...preferences?.youtube?.videoStyle?.selectedPredefinedTags];
+      // console.log(videoStyle, "videoStyle");
       //const specificInstructions = preferences?.customInterestTags?.[1] || ''; // assume string
       const duration = [...preferences?.youtube?.instructionTags];
       console.log(duration, "duration");
